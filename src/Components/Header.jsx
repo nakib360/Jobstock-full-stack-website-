c422124e5520b9f50de92093018dfde0b92b58a1
nakib360 <fliptechofficial@gmail.com>
2025-09-13 10:27:54 +0600

    giving motion transition and make header cool

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,22 @@
 import pen from "../assets/pen.png"
 import note from "../assets/note.png"
 import cv from "../assets/cv.png"
+import * as motion from "motion/react-client"
+import "../index.css"
 
 const Header = () => {
     return (
-        <div className="relative min-h-screen">
+        <div className="relative min-h-screen font-roboto bg-gray-200 bg-grid">
             {/* Background images */}
-            <img className="absolute -rotate-90 bottom-0 z-20" src={pen} alt="" />
-            <img className="absolute -rotate-10 bottom-0 z-10" src={note} alt="" />
-            <img className="absolute right-0 bottom-0 z-20 h-140" src={cv} alt="" />
+            <motion.img animate={{ x: 290, y: 120}} transition={{delay: 0.3, duration: 1 }} className="absolute -rotate-90 -left-50 -bottom-80 z-20" src={pen} alt="" />
+            <motion.img animate={{ y: -230 }} transition={{ duration: 1 }} className="absolute -rotate-10 -bottom-80 z-10" src={note} alt="" />
+            <motion.img animate={{ rotate: -20 }} transition={{ duration: 1 }} className="absolute -right-30 -bottom-30 z-20 h-140" src={cv} alt="" />
 
             {/* Title */}
-            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 text-center z-30">
-                <p className="text-5xl md:text-6xl font-bold">
-                    Find the great jobs offer for you
-                </p>
+            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 space-y-4 text-center z-30">
+                <div className="badge badge-success rounded-full py-4 px-5 bg-[#0b8260] text-white border-none">Get Your Hot Jobs</div>
+                <p className="text-6xl font-semibold">Find the great jobs offer for you</p>
+                <p className="text-xl">Getting a new job is never easy. Check what new jobs we have in store for you on JobStock.</p>
             </div>
         </div>
     );
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router";
-
+import "../index.css";
 const MainLayout = () => {
   return (
-    <div className="min-h-screen bg-white text-black font-Jakarta">
+    <div className="min-h-screen bg-white text-black roboto overflow-x-hidden">
       <Outlet/>
     </div>
   );
